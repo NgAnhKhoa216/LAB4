f57f8544af13f78fafd52108b55152b96e8e04cd
RandomNamez <khoakhung2018@gmail.com>
2023-05-25 14:09:07 +0700

    Add project files.

---
+++ WebApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace WebApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
